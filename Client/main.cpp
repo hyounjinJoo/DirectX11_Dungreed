098ce8f48fc474e753559b9469301a63ae8404ea
주현진 <hyounjin.dev@gmail.com>
2023-02-15 17:01:12 +0900

    디버그 플래그 설정
    Project : Client
    
    [Modify]
    M1. main.cpp : 메모리 누수 확인을 위한 디버그 플래그 설정

--- Client/main.cpp
+++ Client/main.cpp
@@ -34,8 +34,8 @@ int APIENTRY wWinMain(_In_ HINSTANCE hInstance,
 {
     UNREFERENCED_PARAMETER(hPrevInstance);
     UNREFERENCED_PARAMETER(lpCmdLine);
-
-    // TODO: 여기에 코드를 입력합니다.
+    _CrtSetDbgFlag(_CRTDBG_ALLOC_MEM_DF | _CRTDBG_LEAK_CHECK_DF);
+    //_CrtSetBreakAlloc();
 
     // 전역 문자열을 초기화합니다.
     LoadStringW(hInstance, IDS_APP_TITLE, szTitle, MAX_LOADSTRING);
